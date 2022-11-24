a61bc9afa32441b4c9dd79d755c9d2cd2864ff64
Cemre DİŞLİ <72667928+BCemreD@users.noreply.github.com>
2022-11-24 09:32:15 +0300

    Add files via upload

---
+++ test/src/main/java/e_bebek/test/App.java
@@ -0,0 +1,13 @@
+package e_bebek.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
